685c7cb83254d596f81827924c8183f56f40b6c6
Amatul Saboor Siddiqua <amatulsaboor221@gmail.com>
2021-11-26 15:57:20 +0500

    remove extra code

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import User from './components/user/User.js'
 import React from "react";
-import {useState} from 'react';
 import useFetch from './custom-hooks/useFetch';
-import Modal from 'react-modal';
 
 function App() {
   const {data:users, isLoading, error, handleDelete, handleEdit, handleCreate} = useFetch('http://localhost:5500');
-  const [open, setOpen] = useState(true)
   console.log(`after callback`);
-  function closeModal() {
-    setOpen(false);
-  }
+
   return (
     <div>
       {/* <Modal isOpen={open} onRequestClose={closeModal}> <h1>hello</h1><button>Submit</button> </Modal> */}
